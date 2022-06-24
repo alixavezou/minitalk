27ff30a047a488c589153040f6877a7b50effc15
alixavezou <73618982+alixavezou@users.noreply.github.com>
2022-06-24 18:34:30 +0200

    second version

--- client.c
+++ client.c
@@ -3,20 +3,45 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/wait.h>
+#include <signal.h>
+#include <limits.h>
 
-void	send_string(pid_t	pid, int c) // cette fonction va servir à envoyer le PID & la string passée en param du client vers le serveur
+// cette fonction va s'occuper de transformer la string en binaire pour que ce soit compréhensible pour le serveur
+int	ft_char_becomes_binary(pid_t pid, char a)
 {
+	int	i;
+	int bit;
 
+	i = 7;
+	bit = 0;
+	while (i >= 0)
+	{
+		if ((a >> i) & 1)
+			bit = kill(pid, SIGUSR1);
+		else
+			bit = kill(pid, SIGUSR2);
+		if (bit)
+		{
+			write(1, "error\n", 7);
+			exit(1);
+		}
+		usleep(200);
+		i--;
+	}
 }
 
 int main(int argc, char **argv)
 {
 	int		i;
 	pid_t	pid;
+	//char	c;
 
-	i = 0;
+	//pid = 123;
+	//c = 'L';
+	//i = ft_char_becomes_binary(pid, c);
 	if (argc != 3)
 		exit(1);
 }
 
-// il va falloir créer une fonction qui gère les erreurs 
+// il va falloir créer une fonction qui gère les erreurs
+// verifier que le PID != 0 ou à -1
