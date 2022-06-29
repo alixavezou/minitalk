4f7b243e13d04155917c0389655efc062853e862
alixavezou <73618982+alixavezou@users.noreply.github.com>
2022-06-29 14:45:37 +0200

    hdhdh

--- server_resources/server.c
+++ server_resources/server.c
@@ -7,30 +7,23 @@
 #include <signal.h>
 
 // On va utiliser une variable globale dans laquelle on va stocker tous les signaux qu'on recevra
-
-// Cette fonction va servir à déterminer le comportement en fonction de SIGUSR1 ou SIGUSR2
 // Puis elle transfèrera dans la variable globale le signal qu'elle reçoit
-// Puis convertir les int en char (maybe avec ITOA)
 
-void my_handler0(int signal)
+void my_handler(int signal)
 {
-    write(1, "0", 1);
-}
-
-void my_handler1(int signal)
-{
-    write(1, "1", 1);
+    if (signal == SIGUSR1)
+		write(1, "1", 1);
+	else
+		write(1, "0", 1);
 }
 
 int main(void)
 {
 	struct sigaction sa;
-	struct sigaction so;
 
-	sa.sa_handler = my_handler0;
-	so.sa_handler = my_handler1;
+	sa.sa_handler = my_handler;
 	sigaction(SIGUSR2, &sa, NULL);
-	sigaction(SIGUSR1, &so, NULL);
+	sigaction(SIGUSR1, &sa, NULL);
 	printf("pid: %d\n", getpid());
 	while (1)
 		pause();
