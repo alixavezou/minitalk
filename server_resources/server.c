0f7a747e90e85aa98f2c7880b89dae0de319ca4f
alixavezou <73618982+alixavezou@users.noreply.github.com>
2022-07-01 19:05:47 +0200

    serveurrr

--- server_resources/server.c
+++ server_resources/server.c
@@ -6,9 +6,30 @@
 #include <limits.h>
 #include <signal.h>
 
-// On va utiliser une variable globale dans laquelle on va stocker tous les signaux qu'on recevra
-// Puis elle transfèrera dans la variable globale le signal qu'elle reçoit
-char *str = NULL;
+char *str = NULL; // On va utiliser une variable globale dans laquelle on va stocker tous les char qu'on recevra
+
+// fonction qui va (re)transformer le binaire correspondant à la taille de ma string en int donc on fait l'inverse
+void	ft_binary_becomes_int(int signum, int i) //le i s'incrémente à chaque signal reçu
+{
+	static int	length;
+
+	length = 0;
+	if (i < 32)
+	{
+		length = length << 1;
+		if (signum == SIGUSR1)
+			length = length + 1;
+		if (signum == SIGUSR2)
+			length = length + 0;
+	}
+	if (i == 31) // ca y est on a 1 int
+	{
+		str = malloc(sizeof(char) * (length + 1)); // on transmet à notre variable globale la string
+		if (!str)
+			return ;
+		length = 0; // on remet à 0 notre length pour accueillir la prochaine string
+	}
+}
 
 void my_handler(int signal)
 {
@@ -16,6 +37,18 @@ void my_handler(int signal)
 		write(1, "1", 1);
 	else
 		write(1, "0", 1);
+}
+
+void	ft_signals(int signum)
+{
+	static int	i; // i = le nombre de signaux que je reçois
+
+	i = 0;
+	if (i < 32) //32 bites = la taille de la string transmise
+		ft_binary_becomes_int(signum, i);
+	//else
+		//ft_binary_becomes_char(signum, i);
+	i++;
 }
 
 int main(void)
