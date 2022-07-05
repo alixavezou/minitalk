ffc38f0bb0307c71be6d807b95f9e4357ce1d209
alixavezou <73618982+alixavezou@users.noreply.github.com>
2022-07-05 16:34:08 +0200

    kikiki

--- client_resources/client.c
+++ client_resources/client.c
@@ -1,6 +1,5 @@
 # include "client.h"
 
-// cette fonction va s'occuper de transformer la string en binaire pour que ce soit compréhensible pour le serveur
 void	ft_char_becomes_binary(char a, pid_t pid)
 {
 	int	i;
@@ -24,15 +23,13 @@ void	ft_char_becomes_binary(char a, pid_t pid)
 		usleep(150);
 		i--;
 	}
-	usleep(300000);
 }
-// cette fonction va transformer l'int correspondant à la taille de ma string en binaire pour la passer au serveur
-void	ft_int_becomes_binary(int a, pid_t	pid)
+void	ft_int_becomes_binary(int a, pid_t pid) // int a = la taille de ma string
 {
 	int	i;
 	int	bit;
 
-	i = 31; // 1 int = 32 bits
+	i = 31;
 	bit = 0;
 	while (i >= 0)
 	{
@@ -50,7 +47,6 @@ void	ft_int_becomes_binary(int a, pid_t	pid)
 		usleep(150);
 		i--;
 	}
-	//usleep(300000);
 }
 
 void	ft_check_pid(char *str)
@@ -75,7 +71,6 @@ void	ft_errors(int argc, char **argv)
 	int	i;
 
 	i = 0;
-	// il faut d'abord s'assurer que le nb d'args est bien = à 3
 	if (argc != 3)
 	{
 		write(1, "wrong numbers of arguments\n", 28);
@@ -93,8 +88,6 @@ void	ft_errors(int argc, char **argv)
 	}
 	ft_check_pid(argv[1]);
 }
-// il faudra gérer le cas ou le atoi du PID est != 0 et > INT_MAX(overflow).
-//Le pid ne peut pas etre = 0. if atoi(argv[1] == 0) = faux.
 
 int main(int argc, char **argv)
 {
@@ -102,19 +95,17 @@ int main(int argc, char **argv)
 	pid_t	pid;
 
 	i = 0;
-	// Il faut transormer le PID en int car il va être transmis en tant que string alors que c'est un int.
-	//Car meme si tu envoies des numeros dans le terminal l'argument 1 sera considéré comme une chaine de char
 	ft_errors(argc, argv);
 	pid = atoi(argv[1]);
-	if (argc == 3) // donc, si j'ai bien mon executable + mes 2 args on peut envoyer un signal à mon serveur
+	if (argc == 3)
 	{
 		ft_int_becomes_binary(ft_strlen(argv[2]), pid);
-		while(argv[2][i] != '\0') // tant qu'il y a bien des char dans ma string donc dans mon 2e argument après le PID
+		while(argv[2][i] != '\0')
 		{
-			ft_char_becomes_binary(argv[2][i], pid); // on procede bien a la conversion de ma string
+			ft_char_becomes_binary(argv[2][i], pid);
 			i++;
 		}
-		//ft_char_becomes_binary('\0', pid); // on oublie pas d'inclure le '\0' une fois qu'il n'y a plus de char
+		ft_char_becomes_binary('\0', pid);
 	}
 	return (0);
 }
