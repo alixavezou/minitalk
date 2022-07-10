9eddf6483b6cc6eb3a635e8d4446314af366bfc2
alixavezou <73618982+alixavezou@users.noreply.github.com>
2022-07-10 11:45:17 +0200

    serveur

--- server_resources/server.c
+++ server_resources/server.c
@@ -2,40 +2,18 @@
 #include <signal.h>
 #include <limits.h>
 
-char *str = NULL; // On va utiliser une variable globale dans laquelle on va stocker tous les char qu'on recevra
+char *str = NULL;
 
-//fonction pour afficher la string sur le file descriptor
-void    ft_putstr_fd(char *s, int fd)
+void    ft_putchar_fd(char a, int fd)
 {
-    while (*s)
-        write(fd, s++, 1);
+	write(fd, &a, 1);
 }
 
-void	ft_binary_becomes_int(int signum, int i)
-{
-	static int	length = 0;
-
-	if (i < 32)
-	{
-		length = length << 1;
-		if (signum == SIGUSR1)
-			length = length + 1;
-		if (signum == SIGUSR2)
-			length = length + 0;
-	}
-	if (i == 31) //ca y est on a 1 int
-	{
-		str = malloc(sizeof(char) * (length + 1)); //on transmet à notre variable globale l'int
-		if (!str)
-			return ;
-		length = 0; //on remet à 0 notre length pour accueillir notre prochain int
-	}
-}
-//Maintenant une fois qu'on a la taille en int il faut convertir le binaire en char pour remplir la str
-void	ft_binary_becomes_char(int signum, int bit, int index, int *i)
+void	ft_binary_becomes_char(int signum, int bit, siginfo_t *info)
 {
 	static int	a = 0;
 
+	// printf("bite = %d \n", bit);
 	if (bit < 8)
 	{
 		a = a << 1;
@@ -46,39 +24,28 @@ void	ft_binary_becomes_char(int signum, int bit, int index, int *i)
 	}
 	if (bit == 7)
 	{
-		if (str)
-		{
-			str[index] = a;
-			if (a == '\0')
-			{
-				ft_putstr_fd(str, 1);
-				free(str);
-				str = NULL;
-				*i = -1;
-			}
-			a = 0;
-		}
+		ft_putchar_fd(a, 1);
+		a = 0;
 	}
+	kill(info->si_pid, SIGUSR1);
+	//usleep(30);
 }
 
-void	ft_signals_handler(int signum, siginfo_t *info, void *name) //Commonly, the handler function doesn't make any use of the third argument.
+void	ft_signals_handler(int signum, siginfo_t *info, void *name)
 {
-	static int	i = 0; //i = le nombre de signaux que je reçois
-	(void)info;
+	static int	count_bit = 0;
 	(void)name;
+	// printf("$$$ le client me parle!!! \n");
 
-	if (i < 32) //32 bites = la taille de la string transmise
-		ft_binary_becomes_int(signum, i);
-	else
-		ft_binary_becomes_char(signum, (i % 8), ((i - 32) / 8), &i);
-	i++;
+	ft_binary_becomes_char(signum, (count_bit % 8), info);
+	count_bit++;
 }
 
 int main(void)
 {
 	struct sigaction sa;
 
-	printf("pid: %d\n", getpid());
+	printf("server's pid: %d\n", getpid());
 	sa.sa_sigaction = ft_signals_handler;
 	sa.sa_flags = SA_SIGINFO;
 	sigaction(SIGUSR1, &sa, NULL);
---
+++ server_resources/server.h
@@ -0,0 +1,14 @@
+#ifndef SERVER_H
+# define SERVER_H
+
+# include <signal.h>
+# include <limits.h>
+# include "../ft_printf/ft_printf.h"
+
+// struct		s_letter
+// {
+// 	char	character;
+// 	int		count_bit;
+// };
+
+#endif
