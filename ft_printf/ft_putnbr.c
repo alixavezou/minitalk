3f93af22794849c90dc50cd502044ab84c4387cc
EC2 Default User <ec2-user@ip-172-31-36-251.eu-west-3.compute.internal>
2022-07-12 09:27:02 +0000

    printf

--- ft_printf/ft_putnbr.c
+++ ft_printf/ft_putnbr.c
@@ -38,15 +38,15 @@ void	ft_putnbr(int n)
 
 void	ft_putnbru(unsigned int n)
 {
-	if (n >= 0 && n < 10)
+	if (n > 0 && n < 10)
 	{
 		ft_putchar(n + '0');
 	}
-	else if (n < 0)
-	{
-		ft_putchar('-');
-		ft_putnbru(n * (-1));
-	}
+	// else if (n < 0)
+	// {
+	// 	ft_putchar('-');
+	// 	ft_putnbru(n * (-1));
+	// }
 	else
 	{
 		ft_putnbru(n / 10);
